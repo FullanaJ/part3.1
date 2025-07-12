e3a9b30fdd10e5a4d56a8d5ca4c0ee3978fbff1c
FullanaJ <Jorgefullana3@gmail.com>
2025-07-12 18:22:59 +0200

    new dist

--- phonebook/index.js
+++ phonebook/index.js
@@ -34,6 +34,11 @@ let persons = [
       "id": 4,
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
+    },
+    { 
+      "id": 5,
+      "name": "Jorge", 
+      "number": "39-23-6423122"
     }
 ]
 
